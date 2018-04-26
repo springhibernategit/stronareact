57982748daed6146ea3c900c53db47a625c0739a
Michal <sowinskim@interia.eu>
2018-04-26 21:46:19 +0200

    table added from backend

--- src/components/FullRoster.js
+++ src/components/FullRoster.js
@@ -1,9 +1,45 @@
 import React from 'react'
 
-const FullRoster = () => (
-    <div>
-        <p>full roster</p>
-    </div>
-)
+export default class FullRoster extends React.Component {
 
-export default FullRoster
+    constructor(props) {
+        super();
+        this.state = {
+            computerGames: []
+        };
+    }
+
+    componentDidMount() {
+
+        fetch('http://localhost:8080/games')
+            .then(response => response.json())
+            .then(data => this.setState({
+                computerGames: data
+            }));
+    }
+
+    render() {
+        return(
+            <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.computerGames.map(function(p, index) {
+                    return(
+                        <tr>
+                            <td>{p.id}</td>
+                            <td>{p.name}</td>
+                            <td>{p.price}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        );
+    }
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => (
     <header>
         <h1>Tralalal</h1>
-        <nav className = "menu navigation-menu">
+        <nav className = "menu2">
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/roster'>Roster</Link></li>
